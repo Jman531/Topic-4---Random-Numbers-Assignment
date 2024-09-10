5e16b46ab3fdbbe53dbb7195cb853646374f1a2c
Jack Gallagher <339690067@GoTVDSB.ca>
2024-09-10 08:30:34 -0400

    Add project files.

---
+++ Topic 4 - Random Numbers Assignment/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Topic_4___Random_Numbers_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
